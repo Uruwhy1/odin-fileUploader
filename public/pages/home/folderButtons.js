e5134a2b3c78c6555832e3fb3c1823c54d39f7df
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2024-10-02 15:40:59 -0300

    style renaming 'save' button

--- public/pages/home/folderButtons.js
+++ public/pages/home/folderButtons.js
@@ -46,7 +46,8 @@ document.addEventListener("DOMContentLoaded", () => {
       folderElement.replaceChild(inputField, folderNameElement);
 
       const saveButton = document.createElement("button");
-      saveButton.textContent = "Save";
+      saveButton.innerHTML =
+        '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg>';
       saveButton.classList.add("save-rename-btn");
       folderElement.appendChild(saveButton);
 
